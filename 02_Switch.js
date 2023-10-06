379bca30ffb91dff8f62b991cd983e0c9fdb7aa7
Abhishek Gautam <121427611+abhishekgautam95@users.noreply.github.com>
2023-10-06 13:29:38 +0530

    Add files via upload

---
+++ 02_Switch.js
@@ -0,0 +1,29 @@
+//Switch Statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January",);
+        break;
+        case 2:
+            console.log("Febuary",);
+            case 3:
+                console.log("March",);
+                case 4:
+                    console.log("April",);
+                    case 5:
+                        console.log("May",);
+                        case 6:
+                            console.log("june",);
+    default:
+        console.log("Default case matched",);
+        break;
+}
